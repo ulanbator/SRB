2ea246855315c9ee4e920e94f48b6fe6d3d8b23f
fangzheng <18843426103@163.com>
2022-04-24 17:21:24 +0800

    在srb-base模块中统一配置Swagger

--- service-core/src/main/java/com/example/srb/core/ServiceCoreApplication.java
+++ service-core/src/main/java/com/example/srb/core/ServiceCoreApplication.java
@@ -3,12 +3,14 @@ package com.example.srb.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 /**
  * @author fangzheng
  */
 @SpringBootApplication
+@EnableSwagger2
 @ComponentScan({"com.example.srb"})
 public class ServiceCoreApplication {
 
